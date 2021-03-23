776dbd6a47515b41feb91ae48f5573851aa7153d
Ruph-QD <ruphil.lacourt@gmail.com>
2021-03-23 13:56:07 +0100

    accueil php

---
+++ pages/Header.php
@@ -0,0 +1,10 @@
+
+<nav class="main-navigation">
+    <!-- TODO: mettre les liens pour les pages -->
+    <div><a href="Accueil.php"><img class="logo-image logo-image-infinite"src="../assets/images/infinite-mesures.png"></a></div>
+    <div><a href="" class="navigation-element">Forum</a></div>
+    <div><a href="" class="navigation-element">FAQ</a></div>
+    <div><a href="" class="navigation-element">Contact</a></div>
+    <div><a href="" class="navigation-element">A Propos</a></div>
+    <div><a href=""><img class="logo-image logo-image-user" src="../assets/images/logo_user.png"></a></div>
+</nav>
