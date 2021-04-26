f208a27c56462c0fb509641b5a2b6d9ae06a9147
Ruph-QD <ruphil.lacourt@gmail.com>
2021-04-26 10:33:54 +0200

    accueil bug resolution first slide

--- pages/Accueil.php
+++ pages/Accueil.php
@@ -4,7 +4,6 @@
 <head>
   <link rel="stylesheet" href="../style/Template.css" />
   <link rel="stylesheet" href="../style/Accueil.css" />
-  <script type="text/javascript" src="../JavaScript/Accueil.js"></script>
   <meta charset="utf-8" />
   <title>Runnest</title>
 </head>
@@ -13,35 +12,28 @@
 </header>
 
 <body>
-  <!-- Slideshow container -->
   <div class="slideshow-container">
 
-    <!-- Full-width images with number and caption text -->
     <div class="mySlides fade">
-      <div class="numbertext">1 / 3</div>
       <img class="image" src="../assets/images/image_runner-1.png">
-      <div class="text">Caption Text</div>
+      <div class="text"></div>
     </div>
 
     <div class="mySlides fade">
-      <div class="numbertext">2 / 3</div>
       <img class="image" src="../assets/images/image_runner-2.png">
-      <div class="text">Caption Two</div>
+      <div class="text"></div>
     </div>
 
     <div class="mySlides fade">
-      <div class="numbertext">3 / 3</div>
       <img class="image" src="../assets/images/image_runner-3.png">
-      <div class="text">Caption Three</div>
+      <div class="text"></div>
     </div>
 
-    <!-- Next and previous buttons -->
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
   </div>
   <br>
 
-  <!-- The dots/circles -->
   <div style="text-align:center">
     <span class="dot" onclick="currentSlide(1)"></span>
     <span class="dot" onclick="currentSlide(2)"></span>
@@ -49,6 +41,9 @@
   </div>
 
 </body>
+
+<script type="text/javascript" src="../JavaScript/Accueil.js"></script>
+
 <footer>
   <?php include("./Component/Footer.php"); ?>
 </footer>
