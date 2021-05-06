d0dc011e9fec30f1b20145b0710ba4e8165d67b6
Ruph-QD <ruphil.lacourt@gmail.com>
2021-05-06 11:54:28 +0200

    interrective dot

--- JavaScript/Accueil.js
+++ JavaScript/Accueil.js
@@ -5,7 +5,9 @@ let slideInterval = setInterval(() => changeSlide(true), slideTime);
 
 function initialize() {
   const slides = document.getElementsByClassName('slide');
+  const dots = document.getElementsByClassName('dot');
   slides[0].style.display = "block";
+  dots[0].classList.add("active");
 }
 
 function jumpSlide(forward) {
@@ -14,23 +16,41 @@ function jumpSlide(forward) {
   slideInterval = setInterval(() => changeSlide(true), slideTime);
 }
 
+function currentSlide(index) {
+  clearInterval(slideInterval);
+  const slides = document.getElementsByClassName('slide');
+  const dots = document.getElementsByClassName('dot');
+  slides[slideIndex].style.display = "none";
+  dots[slideIndex].classList.remove("active");
+  slideIndex = index;
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].classList.add("active");
+  slideInterval = setInterval(() => changeSlide(true), slideTime);
+}
+
 function changeSlide(forward) {
   const slides = document.getElementsByClassName('slide');
+  const dots = document.getElementsByClassName('dot');
   slides[slideIndex].style.display = "none";
+  dots[slideIndex].classList.remove("active");
   if (forward) {
     if (slideIndex + 1 > slides.length - 1) {
       slides[0].style.display = "block";
+      dots[0].classList.add("active");
       slideIndex = 0;
     } else {
       slides[slideIndex + 1].style.display = "block";
+      dots[slideIndex + 1].classList.add("active");
       slideIndex ++;
     } 
   } else {
     if (slideIndex - 1 < 0) {
       slides[slides.length - 1].style.display = "block";
+      dots[dots.length - 1].classList.add("active");
       slideIndex = slides.length - 1;
     } else {
       slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].classList.add("active");
       slideIndex --;
     }
   }
