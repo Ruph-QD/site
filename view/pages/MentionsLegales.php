d67f67204670d1ab5ea590848925c1bc6cd49e06
carlajourno <76062317+carlajourno@users.noreply.github.com>
2021-05-27 10:04:13 +0200

    fix: création page mentions légales

---
+++ view/pages/MentionsLegales.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="MentionsLegales.css">
+        <title>Mentions légales</title>
+    </head>
+
+    <body>
+        <header>
+            <?php include("./Component/newHeader.php"); ?>
+        </header>
+
+        <center><h1 class="mentions-legales">MENTIONS LEGALES</h1><br></center>
+        
+        <h4 class="titres">EDITEUR DU SITE</h4>
+        <p class="paragraphes">RUNNEST, 28 rue Notre Dame des Champs, 75006 PARIS – SIRET – APE</p>
+        <h4 class="titres">DÉCLARATION A LA CNIL</h4>
+        <p class="paragraphes">
+            Conformément à la Loi Informatique et Libertés, 
+            nous vous informons que la collecte de données personnelles associée à ce site 
+            a fait l’objet d’’une déclaration de traitement automatisé d’’informations nominatives 
+            auprès de la CNIL.
+        </p>
+        <h4 class="titres">COLLECTE DE DONNEES NOMINATIVES</h4>
+        <p class="paragraphes">
+            Par donnée nominative nous entendons toute information qui permet de vous identifier 
+            soit directement soit indirectement.
+        </p>
+        <h4 class="titres">LES PROFILS</h4>
+        <p class="paragraphes">
+            Nous recueillons des données nominatives par l’intermédiaire de profil associé à votre compte 
+            et tout envoi d’informations via la messagerie interne. Les informations recueillies 
+            font l’objet d’un traitement informatique destiné à vous informer et répondre à votre demande.
+        </p>
+        <h4 class="titres">DIVULGATION DES DONNEES</h4>
+        <p class="paragraphes">
+            INFINITE MEASURES s’engage à garantir le respect de la vie privée des internautes 
+            qui visitent ce site web et à veiller dans les limites de l’état de l’art à la confidentialité des 
+            informations personnelles qui lui sont transmises. Les destinataires des données sont INFINITE MEASURES 
+            et ses collaborateurs uniquement. Notez que le choix d'un coach ou d'un coureur entraîne l'acceptation 
+            du partage de vos données personnelles à celui-ci.
+        </p>
+        <h4 class="titres">DROITS D’ACCES, DE MODIFICATION ET DE SUPPRESSION DES DONNEES</h4>
+        <p class="paragraphes">
+            Conformément à la loi «informatique et libertés» du 6 janvier 1978, vous bénéficiez d’un droit d’accès 
+            et de rectification aux informations qui vous concernent. Si vous souhaitez exercer ce droit et obtenir 
+            communication des informations vous concernant, veuillez adresser un courrier à nos administrateurs via 
+            la messagerie interne, en joignant une photocopie de votre carte d’identité.
+            Vous pouvez également, pour des motifs légitimes, vous opposer au traitement des données vous concernant.
+        </p>
+        <h4 class="titres">COOKIES</h4>
+        <p class="paragraphes">
+            Vous êtes visiteur du site web, Vous pouvez consulter une partie du site sans avoir à vous identifier, 
+            à savoir les rubriques "FAQ" et "A propos". 
+            Nous respectons l’anonymat de nos visiteurs. Nous n’établissons pas de profil des visiteurs
+            Les données de connexion au site (fichiers log) sont utilisées uniquement pour la sécurité du site 
+            (détection d’éventuelles intrusions). Nous utilisons des cookies persistants (Un cookie est un petit bloc de données 
+            envoyé par un serveur web et stocké sur le disque dur de votre ordinateur).
+            Les cookies que nous utilisons (si nécessaire) ne permettent pas de vous identifier. Ils nous permettent 
+            juste de mémoriser les caractéristiques d’un utilisateur durant l’une de ses visites.
+            Dès que vous quitterez notre site web, le cookie que nous avons généré va disparaître du disque dur de votre ordinateur.
+            Notre objectif n’est pas de reconnaître votre machine lors de vos consultations ultérieures.
+            Vous pouvez refuser les cookies en configurant votre navigateur, notre site vous restera accessible. 
+            En modifiant les options de votre navigateur, vous pouvez demander d’être averti lors de l’activation d’un cookie, 
+            ou refuser l’ensemble des cookies. Si vous choisissez cette dernière option, notre site web vous restera accessible, 
+            mais il est possible que votre navigation soit moins confortable.
+        </p>
+        <h4 class="titres">CHAMP D’APPLICATION</h4>
+        <p class="paragraphes">
+            La présente ne s’applique qu’aux données collectées sur ce Site Web. Ce dernier peut contenir des liens vers d’autres sites. 
+            Veuillez noter que INFINITE MEASURES n’exerce aucun contrôle ni aucune influence sur les polices de protection des autres sites. 
+            Nous encourageons nos utilisateurs à être attentifs, lorsqu’ils quittent le Site Web, et à lire les chartes de protection 
+            des données personnelles de chacun des autres sites qu’ils visitent.
+        </p>
+        <h4 class="titres">ACCEPTATION</h4>
+        <p class="paragraphes">
+            En utilisant ce site Web, vous acceptez implicitement les termes de notre politique de traitement des données personnelles 
+            et vous nous autorisez à traiter ces données conformément aux objectifs énoncés ci-dessus.
+        </p>
+        <h4 class="titres">MISE A JOUR</h4>
+        <p class="paragraphes">
+            Nous nous réservons le droit de modifier périodiquement cette page. Veuillez de ce fait lire régulièrement les présentes.
+        </p>
+
+        <footer>
+            <?php include("./Component/Footer.php"); ?>
+        </footer>
+    </body>
+</html>
